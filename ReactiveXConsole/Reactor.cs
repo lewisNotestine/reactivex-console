aa59c0ed7a687144a1447faf851e1ab28df30df2
Lewis Notestine <lewis.notestine@gmail.com>
2019-04-10 01:26:14 -0700

    clean things up a bit for showing

--- ReactiveXConsole/Reactor.cs
+++ ReactiveXConsole/Reactor.cs
@@ -10,47 +10,52 @@ namespace ReactiveXConsole
 {
     internal class Reactor
     {
+        // Subjects are Both Observer and Observable
+        // That is, they can generate both behavior and projections of behavior.
         private readonly ISubject<string> _distinctInput;
-        private readonly IObservable<string> _distinctObservable;
-
         private readonly ISubject<string> _oneInput;
-        private readonly IObservable<string> _oneObservable;
-
         private readonly ISubject<string> _twoInput;
-        private readonly IObservable<string> _twoObservable;
-
-        private readonly IObservable<string> _mergeObservable;
-
         private readonly ISubject<string> _debounceInput;
+
+        private readonly IObservable<string> _distinctObservable;
+        private readonly IObservable<string> _oneObservable;
+        private readonly IObservable<string> _twoObservable;
+        private readonly IObservable<string> _mergeObservable;
         private readonly IObservable<string> _debounceObservable;
+
 #region what_about...
         // things to try:
         // 1. what about switching up numbers input (i.e., state as stream)
         // 2. what about merging?
 #endregion
+
         public Reactor()
         {
             _distinctInput = new BehaviorSubject<string>("");
             _distinctObservable = _distinctInput
-                .DistinctUntilChanged();
-            _distinctObservable.Subscribe(s => Console.WriteLine($"got distinct {s}"));
+                .DistinctUntilChanged()
+                .Select(s => $"got distinct {s}");
+            _distinctObservable.Subscribe(s => Console.WriteLine(s));
 
             _oneInput = new BehaviorSubject<string>("");
             _oneObservable = _oneInput
-                .Where(s => s.Equals("1", StringComparison.InvariantCultureIgnoreCase));
-            _oneObservable.Subscribe(s => Console.WriteLine($"got 1"));
+                .Where(s => s.Equals("1", StringComparison.InvariantCultureIgnoreCase))
+                .Select(s => $"got {s}");
+            _oneObservable.Subscribe(s => Console.WriteLine(s));
 
             _twoInput = new BehaviorSubject<string>("");
             _twoObservable = _twoInput
-                .Where(s => s.Equals("2", StringComparison.InvariantCultureIgnoreCase));
-            _twoObservable.Subscribe(s => Console.WriteLine("got 2"));
+                .Where(s => s.Equals("2", StringComparison.InvariantCultureIgnoreCase))
+                .Select(s => $"got {s}");
+            _twoObservable.Subscribe(s => Console.WriteLine(s));
 
             _mergeObservable = Observable.CombineLatest(_oneObservable, _twoObservable)
-                .Select<IList<string>, string>(l => $"got both {l[0]} and {l[1]}");
+                .Select<IList<string>, string>(l => $"both {l[0]} and {l[1]}");
             _mergeObservable.Subscribe(s => Console.WriteLine(s));
 
             _debounceInput = new BehaviorSubject<string>("");
             _debounceObservable = _debounceInput
+                .Where(s => !string.IsNullOrEmpty(s))
                 .Throttle(TimeSpan.FromSeconds(3))
                 .Select(s => $"bouncy bouncy {s}");
             _debounceObservable.Subscribe(s => Console.WriteLine(s));
@@ -71,12 +76,9 @@ namespace ReactiveXConsole
             _debounceInput.OnNext(input);
         }
 
-
         public void HandleDistinctUntilChanged(string input)
         {
             _distinctInput.OnNext(input);
         }
-
     }
-
 }
